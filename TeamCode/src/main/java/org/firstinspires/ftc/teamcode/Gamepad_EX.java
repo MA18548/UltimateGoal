052534c100337cff61c2c6e020d2a28cf9da5aaf
konata-chan404 <42537566+konata-chan404@users.noreply.github.com>
2021-02-17 13:28:43 +0200

    aaaaa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad_EX.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad_EX.java
@@ -32,24 +32,94 @@ public class Gamepad_EX
     {
         this.gamepad = gamepad;
 
-        this.A = new Trigger(() -> this.gamepad.a);
-        this.B = new Trigger(() -> this.gamepad.b);
-        this.X = new Trigger(() -> this.gamepad.x);
-        this.Y = new Trigger(() -> this.gamepad.y);
+        this.A = new Trigger(this::getAButtonAsBool);
+        this.B = new Trigger(this::getBButtonAsBool);
+        this.X = new Trigger(this::getXButtonAsBool);
+        this.Y = new Trigger(this::getYButtonAsBool);
     
-        this.LEFT_BUMPER = new Trigger(() -> this.gamepad.left_bumper);
-        this.RIGHT_BUMPER = new Trigger(() -> this.gamepad.right_bumper);
+        this.LEFT_BUMPER = new Trigger(this::getLeftBumperAsBool);
+        this.RIGHT_BUMPER = new Trigger(this::getRightBumperAsBool);
     
-        this.DPAD_UP = new Trigger(() -> this.gamepad.dpad_up);
-        this.DPAD_DOWN = new Trigger(() -> this.gamepad.dpad_down);
-        this.DPAD_LEFT = new Trigger(() -> this.gamepad.dpad_left);
-        this.DPAD_RIGHT = new Trigger(() -> this.gamepad.dpad_right);
+        this.DPAD_UP = new Trigger(this::getUpDpadAsBool);
+        this.DPAD_DOWN = new Trigger(this::getDownDpadAsBool);
+        this.DPAD_LEFT = new Trigger(this::getLeftDpadAsBool);
+        this.DPAD_RIGHT = new Trigger(this::getRightDpadAsBool);
     
-        this.START = new Trigger(() -> this.gamepad.start);
-        this.BACK = new Trigger(() -> this.gamepad.back);
+        this.START = new Trigger(this::getStartButtonAsBool);
+        this.BACK = new Trigger(this::getBackButtonAsBool);
     
-        this.LEFT_STICK = new Trigger(() -> this.gamepad.left_stick_button);
-        this.RIGHT_STICK = new Trigger(() -> this.gamepad.right_stick_button);
+        this.LEFT_STICK = new Trigger(this::getLeftStickButtonAsBool);
+        this.RIGHT_STICK = new Trigger(this::getRightStickButtonAsBool);
+    }
+
+    public boolean getAButtonAsBool()
+    {
+        return this.gamepad.a;
+    }
+
+    public boolean getBButtonAsBool()
+    {
+        return this.gamepad.b;
+    }
+
+    public boolean getXButtonAsBool()
+    {
+        return this.gamepad.x;
+    }
+
+    public boolean getYButtonAsBool()
+    {
+        return this.gamepad.y;
+    }
+
+    public boolean getLeftBumperAsBool()
+    {
+        return this.gamepad.left_bumper;
+    }
+
+    public boolean getRightBumperAsBool()
+    {
+        return this.gamepad.left_bumper;
+    }
+
+    public boolean getUpDpadAsBool()
+    {
+        return this.gamepad.dpad_up;
+    }
+
+    public boolean getDownDpadAsBool()
+    {
+        return this.gamepad.dpad_down;
+    }
+
+    public boolean getRightDpadAsBool()
+    {
+        return this.gamepad.dpad_right;
+    }
+
+    public boolean getLeftDpadAsBool()
+    {
+        return this.gamepad.dpad_left;
+    }
+
+    public boolean getStartButtonAsBool()
+    {
+        return this.gamepad.start;
+    }
+
+    public boolean getBackButtonAsBool()
+    {
+        return this.gamepad.back;
+    }
+
+    public boolean getLeftStickButtonAsBool()
+    {
+        return this.gamepad.left_stick_button;
+    }
+
+    public boolean getRightStickButtonAsBool()
+    {
+        return this.gamepad.right_stick_button;
     }
 
     public double getLeftY()
